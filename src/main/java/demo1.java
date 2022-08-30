69d271dc2e9240c9cc9a5034d457bba61e017944
wyh <2270318347@qq.com>
2022-08-30 23:44:48 +0800

    冲突合并

--- src/main/java/demo1.java
+++ src/main/java/demo1.java
@@ -9,5 +9,6 @@ public class demo1 {
         System.out.println("dev one");
         System.out.println("three");
         System.out.println("dev three");
+        System.out.println("push");
     }
 }
