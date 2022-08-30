589b4c3d1d1c3212a918d3c8261e3cd94eeae1c5
wyh <2270318347@qq.com>
2022-08-30 20:59:12 +0800

    dev第一次

--- src/main/java/demo1.java
+++ src/main/java/demo1.java
@@ -6,5 +6,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("one");
         System.out.println("two");
+        System.out.println("dev one");
     }
 }
